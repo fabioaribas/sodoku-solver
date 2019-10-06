4c6af7f36fa966281b8531da37eec8e090e83830
Fabio Almeida Ribas <fabioaribas@gmail.com>
2019-10-06 01:06:52 -0300

    initial functions

---
+++ sodoku_solver.py
@@ -0,0 +1,112 @@
+>>> M = [
+	[0,0,3, 0,0,0, 5,0,0],
+	[0,0,0, 1,2,8, 0,0,0],
+	[2,0,0, 0,0,0, 0,0,6],
+	[0,3,0, 5,0,9, 0,1,0],
+	[0,6,0, 0,8,0, 0,4,0],
+	[0,9,0, 3,0,7, 0,2,0],
+	[4,0,0, 0,0,0, 0,0,9],
+	[0,0,0, 9,1,6, 0,0,0],
+	[0,0,6, 0,0,0, 2,0,0]
+	]
+	
+	
+def startPos(numBlock):
+	#9x9 matrix is divided into 9 blocks (3x3) from 0 to 8
+	iPos = (numBlock // 3)*3
+	jPos = (numBlock * 3) % 6
+	vector = [iPos,jPos]
+	return(vector)
+	
+def retInitValues(numBlock):
+	initValues = []
+	startp = startPos(numBlock)
+	for i in range(startp[0], startp[0]+3):
+		for j in range(startp[1], startp[1]+3):
+			if (M[i][j] != 0):
+				initValues = initValues + [M[i][j]]
+	return initValues
+	
+def retValues(numBlock):
+	vinit = retInitValues(numBlock)
+	vret = []
+	for k in range(1,10):
+		if not(k in vinit):
+			vret = vret + [k]
+	return vret
+
+def retLineValues(numLine):
+	vRes = [1,2,3,4,5,6,7,8,9]
+	for j in range(0,9):
+		if (M[numLine][j] != 0):
+			vRes.remove(M[numLine][j])
+
+def retColValues(numCol):
+	vRes = [1,2,3,4,5,6,7,8,9]
+	for i in range(0,9):
+		if (M[i][numCol] != 0):
+			vRes.remove(M[i][numCol])
+	return vRes
+
+def listsIntersect(a,b):
+	return list(set(a).intersection(b))
+
+def lists3Intersect(a,b,c):
+	return listsIntersect( listsIntersect(a,b), listsIntersect(b,c) )
+
+def simpleSolveBlock(numBlock):
+	startp = startPos(numBlock)
+	si = startp[0]
+	sj = startp[1]
+
+	blockv = retValues(numBlock)
+	
+	for i in range(si, si+3):
+		reti = retLineValues(i)
+		for j in range(sj, sj+3):
+			
+			if (M[i][j] == 0):
+				retj = retColValues(j)
+				values = lists3Intersect(reti,retj,blockv)
+				print("i:%d j:%d" % (i,j))
+				print(values)
+				if (len(values)==1):
+					print('UPDATED')
+					M[i][j] = values[0]
+					
+
+#MELHOR:
+def simpleSolveBlock(numBlock):
+	startp = startPos(numBlock)
+	si = startp[0]
+	sj = startp[1]
+
+	blockv = retValues(numBlock)
+	
+	for i in range(si, si+3):
+		reti = retLineValues(i)
+		for j in range(sj, sj+3):
+			
+			if (M[i][j] == 0):
+				retj = retColValues(j)
+				values = lists3Intersect(reti,retj,blockv)
+				
+				if (len(values)==1):
+					print("i:%d j:%d %s (UPDATED)" % (i,j,str(values)))
+					M[i][j] = values[0]
+				else:
+					print("i:%d j:%d %s" % (i,j,str(values)))
+					
+def solveBlocks():
+	for k in range(0,9):
+		simpleSolveBlock(k)
+		
+
+		
+quando não tiver um novo UPDATED então tem que buscar uma lista de 2 e supor um número da lista
+depois da suposição verifica se algum caso ficou com [] (0 possibilidades), esse é o caso inválido!
+					
+					
+					
+
+					
